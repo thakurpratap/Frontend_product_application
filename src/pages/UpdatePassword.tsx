6ffc51682442a27359d2a79b0cf6138fdaba6939
Dinesh Prajapati <144621296+dineshprajapati9867@users.noreply.github.com>
2024-11-08 12:19:23 +0530

    Update UpdatePassword.tsx

--- src/pages/UpdatePassword.tsx
+++ src/pages/UpdatePassword.tsx
@@ -7,8 +7,9 @@ const UpdatePassword = () => {
   const [newPassword, setNewPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [message, setMessage] = useState('');
-  const navigate=useNavigate()
-  const handleSubmit = async (e:any) => {
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     if (newPassword !== confirmPassword) {
       setMessage("Passwords do not match!");
@@ -20,25 +21,25 @@ const UpdatePassword = () => {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          Authorization: ` ${localStorage.getItem("emailToken")}`, 
+          Authorization: `${localStorage.getItem("emailToken")}`, 
         },
-        body: JSON.stringify({ password: newPassword }),
+        body: JSON.stringify({ newPassword: newPassword }),
       });
 
       const data = await response.json();
-        console.log(data)
-     
+      console.log(data);
+
       if (response.ok) {
         setMessage("Password updated successfully!");
-        toast.success("Password updated successfully!")
-        navigate("/signin")
+        toast.success("Password updated successfully!");
+        navigate("/signin");
       } else {
         setMessage(data.error || "Failed to update password.");
-        toast.error("Failed to update password.")
+        toast.error("Failed to update password.");
       }
     } catch (error) {
       setMessage("An error occurred. Please try again.");
-      toast.error("Please try again ")
+      toast.error("Please try again.");
     }
   };
 
